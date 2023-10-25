9c1895a3ad406f8607d608fd5b665c04fb27e972
bsegers <briansegs@gmail.com>
2023-10-24 20:42:24 -0400

    Init: Add first file

---
+++ assignment_5_2.py
@@ -0,0 +1,11 @@
+"""Assignment 5.2"""
+
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    print(num)
+
+print("Maximum", largest)
