098f29113998fae7916f65574ae6532c7429bde2
bsegers <briansegs@gmail.com>
2023-10-24 23:25:00 -0400

    Add: Assignment 5.2 (passed)

--- assignment_5_2.py
+++ assignment_5_2.py
@@ -1,11 +1,32 @@
 """Assignment 5.2"""
 
-largest = None
-smallest = None
-while True:
-    num = input("Enter a number: ")
-    if num == "done":
-        break
-    print(num)
+max_val = 0
+min_val = 0
 
-print("Maximum", largest)
+while True:
+    # Write a program that repeatedly prompts a user for integer numbers until
+    # the user enters 'done'.
+    sval = input("Enter a number: ")
+    if sval == "done":
+        break
+
+    # If the user enters anything other than a valid number catch it with a try/except
+    # and put out an appropriate message and ignore the number.
+    try:
+        num = int(sval)
+    except:
+        print("Invalid input")
+        continue
+
+    num = int(sval)
+
+    if num > max_val:
+        max_val = num
+    elif min_val == 0:
+        min_val = num
+    elif num < min_val:
+        min_val = num
+
+# Once 'done' is entered, print out the largest and smallest of the numbers.
+print("Maximum is", max_val)
+print("Minimum is", min_val)
