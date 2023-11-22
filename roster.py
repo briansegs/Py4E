9198c98316f1b55f89bb35e453cc88db3aa2546d
bsegers <briansegs@gmail.com>
2023-11-21 19:00:28 -0500

    add: role to Member

--- roster.py
+++ roster.py
@@ -38,8 +38,9 @@ json_data = json.loads(str_data)
 for entry in json_data:
     name = entry[0]
     title = entry[1]
+    role = entry[2]
 
-    print((name, title))
+    print((name, title, role))
 
     cur.execute('''INSERT OR IGNORE INTO User (name)
                 VALUES (?)''', (name, ))
@@ -52,8 +53,7 @@ for entry in json_data:
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-                (user_id, course_id) VALUES (?, ? )''',
-                (user_id, course_id))
+                (user_id, course_id, role) VALUES (?, ?, ? )''',
+                (user_id, course_id, role))
     conn.commit()
-
     
